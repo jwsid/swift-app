9adfd072b09b3664c52da153fa6085effe6f903e
Justin W Siddons <jwsiddons@gmail.com>
2017-05-10 21:34:18 -0500

    My first commit.

--- SidTwo/ViewController.swift
+++ SidTwo/ViewController.swift
@@ -3,16 +3,29 @@
 //  SidTwo
 //
 //  Created by Justin W Siddons on 5/10/17.
-//  Copyright © 2017 Justin W Siddons. All rights reserved.
+//  Copyright © 2017 Justin W Siddons. All rights reserved...
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            coolLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
