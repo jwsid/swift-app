fabf7db9aa75b9516928ab617a7512cbec196c4b
Justin W Siddons <jwsiddons@gmail.com>
2017-05-11 21:26:58 -0500

    Boolean fun!

--- SidTwo/ViewController.swift
+++ SidTwo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var coolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonTapped(_ sender: Any) {
-        coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        let addition = true
+        
+        if addition {
+            
+            coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            coolLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
         
     }
     
@@ -27,12 +35,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
